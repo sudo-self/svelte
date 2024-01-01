f918d92c8747db2677395e917b810062f3fad2a7
Sudo-Self <sudo-self@proton.me>
2023-12-31 18:22:27 -0700

    Add files via upload

---
+++ routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1><pre>npm create svelte@latest</pre></h1>
+
+	<p>
+		This is&nbsp;<a href="https://kit.svelte.dev">SvelteKit</a>&nbsp;app.
+	</p>
+
+
+	<p>
+		This page purely static HTML
+	</p>
+
+</div>
